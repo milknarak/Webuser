9b0c8dc5c2e4f0a6e92445cfeb2c08a9a9e58c58
Pongkot Sae-li <pongkot.saeli@outlook.com>
2022-03-22 23:23:04 +0700

    feat: add example

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace WebUser.Controllers
         {
             return View();
         }
+        
+        public IActionResult UserdataExample()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
