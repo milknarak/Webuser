70cb4a7005c4afc8440c01c8d88bd3f7e93f142e
Chayutra C <chayutra_c@softsquaregroup.com>
2022-03-22 23:03:47 +0700

    view update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace WebUser.Controllers
             return View();
         }
 
+        public IActionResult Userdata()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
