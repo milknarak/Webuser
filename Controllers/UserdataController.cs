6d91b229e38f3eed70cefc91da85e3480e9f29b2
Chayutra C <chayutra_c@softsquaregroup.com>
2022-03-23 13:10:56 +0700

    create controller add edit delete

--- Controllers/UserdataController.cs
+++ Controllers/UserdataController.cs
@@ -15,16 +15,45 @@ namespace WebUser.Controllers
     public class UserdataController : ControllerBase
     {
         [HttpGet]
-        public List<Userdata> Get()
+        public List<Userdata> GetAllUserdata()
         {
             using var db = new NpgsqlConnection(@"Host=172.16.0.49;Username=postgres;Password=p@ssw0rd;Database=milk_practice");
             var result = db.Query<Userdata>("SELECT * FROM userdata").ToList();
             return result;
         }
+
         [HttpPost]
-        public string Post()
+        public string CreateUserdata(Userdata data)
         {
-            return "POST";
+            using var db = new NpgsqlConnection(@"Host=172.16.0.49;Username=postgres;Password=p@ssw0rd;Database=milk_practice");
+            db.Query<Userdata>(@"
+                INSERT INTO userdata (id, name, lastname)
+                VALUES (@id, @name, @lastname)
+            ", data);
+            return "Create userdata successful";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteUserdata(string id)
+        {
+            using var db = new NpgsqlConnection(@"Host=172.16.0.49;Username=postgres;Password=p@ssw0rd;Database=milk_practice");
+            db.Query<Userdata>(@"
+                DELETE FROM userdata
+                WHERE id = @id
+            "), id);
+            return "Delete userdata suscessful";
+        }
+
+        [HttpPut]
+        public string EditUserdata(string id)
+        {
+            using var db = new NpgsqlConnection(@"Host=172.16.0.49;Username=postgres;Password=p@ssw0rd;Database=milk_practice");
+            db.Query<Userdata>(@"
+            UPDATE username
+            SET id = @id, name = @name, lastname = @lastname
+            WHERE id = @id
+            "), id);
+            return "Edit userdata suscessful";
         }
     }
 }
