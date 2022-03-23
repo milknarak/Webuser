e1e617c81558b08b75d4d07aa0863022634167e1
Pongkot Sae-li <pongkot.saeli@outlook.com>
2022-03-23 09:28:08 +0700

    fix: datatype

--- Models/Userdata.cs
+++ Models/Userdata.cs
@@ -2,7 +2,7 @@ namespace WebUser.Models;
 
 public class Userdata
 {
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public string Lastname { get; set; }
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Lastname { get; set; }
 }
